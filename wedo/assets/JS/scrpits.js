022e0f41e18ee5a3685664f1475576fbbb6100f2
devananthans <devananthan25@gmail.com>
2024-12-31 15:45:25 +0530

    pop Up image and about us rating section

---
+++ wedo/assets/JS/scrpits.js
@@ -0,0 +1,22 @@
+
+var typed = new Typed('.auto-type',
+ {
+  strings: ["Energy-Efficient", "Technology Enabled ", "Eco-Friendly"],
+  typeSpeed: 200,
+  backSpeed: 200,
+  loop: true
+});
+
+
+
+
+
+// Initialization for ES Users
+import { Ripple, initMDB } from "mdb-ui-kit";
+
+initMDB({ Ripple });
+
+// Initialization for ES Users
+import { Carousel, initMDB } from "mdb-ui-kit";
+
+initMDB({ Carousel });
